7f5a6de717c9f1b870f970639ccbddabb1390a72
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-02-16 01:08:46 +0100

    feat: chiamata API e props

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,35 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
 export default {
   name: 'boolfolio',
+  data() {
+    return {
+      projects: [],
+      baseUrl: 'http://127.0.0.1:8000',
+      apiUrls: {
+        projects: '/api/projects'
+      },
+    }
+  },
   components: {
     AppHeader,
     AppMain,
     AppFooter,
 
+  },
+  methods: {
+    getprojects() {
+      axios.get(this.baseUrl + this.apiUrls.projects).then(response => {
+        this.projects = response.data.data;
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  created() {
+    this.getprojects();
   },
 
 };
@@ -19,7 +41,7 @@ export default {
 <!--html-->
 <template>
   <AppHeader />
-  <AppMain />
+  <AppMain :projects=projects />
   <AppFooter />
 </template>
 <!--/html-->
